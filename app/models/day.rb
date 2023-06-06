d8dc38eb30ad60a49e9016949da2e5a669c576e1
Louis Mellac <louismellac@gmail.com>
2023-06-06 12:52:22 +0100

    Generated models and migrations for Category, Day, and Task. Also addded validations and associations to them.

---
+++ app/models/day.rb
@@ -0,0 +1,4 @@
+class Day < ApplicationRecord
+  belongs_to :user
+  has_many :tasks
+end
---
+++ app/models/task.rb
@@ -0,0 +1,8 @@
+class Task < ApplicationRecord
+  belongs_to :day
+  belongs_to :category
+  belongs_to :user, through: :day
+
+  validates :description, presence: true
+  validates :priority, presence: true, numericality: { only_integer: true, in: (1..5) }
+end
