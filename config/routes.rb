7b3245fd83f8cb726e81014262d96b399f8d5184
Louis Mellac <louismellac@gmail.com>
2023-06-06 12:16:05 +0100

    routes for tasks, days, and categories. Root is Devise login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "sessions#new"
+  devise_for :user
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 
   resources :days, only: [:create, :index, :show, :update] do
     resources :tasks, only: [:create, :update]
   end
+
   resources :tasks, only: [:delete]
+
   resources :categories, only: [:create, :update, :delete]
 end
