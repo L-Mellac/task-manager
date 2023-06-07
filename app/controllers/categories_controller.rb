b7827aa31f7dd77a9ac64143ff0f097e03b1fbf8
Su Lynn Eu <eusulynn@gmail.com>
2023-06-07 17:04:26 +0100

    created crud actions for tasks, days, categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,32 @@
 class CategoriesController < ApplicationController
   def create
+    @category = Category.new( name: category_params[:name] )
+    @category.user = current_user
+    # @user = User.find(params[:user_id])
+    # @category.user = @user
+    @day = Day.find(params[:category][:day].to_i)
+    if @category.save
+      flash[:alert] = "Category Created!"
+      redirect_to day_path(@day)
+    else
+      render "days/show", status: :unprocessable_entity
+    end
   end
 
   def update
+    @category = Category.find(params[:id])
+    @category.update(category_params)
+    redirect_to #today
   end
 
   def delete
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to #today
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name, :day)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   devise_for :user
 
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+  # devise_scope :user do
+  #   root to: "devise/sessions#new"
+  # end
 
   resources :days, only: [:create, :index, :show, :update] do
     resources :tasks, only: [:create, :update]
   end
 
-  resources :tasks, only: [:delete]
+  resources :tasks, only: [:destroy]
 
-  resources :categories, only: [:create, :update, :delete]
+  resources :categories, only: [:create, :update, :destroy]
 end
