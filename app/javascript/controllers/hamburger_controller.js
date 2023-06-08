45c3f4204dd86bb78d5011c6c0d1829833f35b94
saroj pun <sarojpun@hotmail.com>
2023-06-08 16:04:18 +0100

    landing page done except the linking

--- app/javascript/controllers/hamburger_controller.js
+++ app/javascript/controllers/hamburger_controller.js
@@ -1,14 +1,14 @@
-// import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus"
 
-// export default class extends Controller {
-//   // static targets = ["menu-bar", "menu-bg", "nav"]
-//   connect() {
-//     console.log("Hello world")
-//   }
-//   // expandMenu() {
-//   //   menu-barTarget.classList.toggle("change");
-//   //   navTarget.classList.toggle("change");
-//   //   menu-bgTarget.classList.toggle("change-bg");
-//   // }
-// }
+export default class extends Controller {
+  static targets = ["menubar", "menubg", "nav"]
+  connect() {
+    // console.log("Hello world")
+  }
 
+  expandMenu() {
+    this.menubarTarget.classList.toggle("change");
+    this.navTarget.classList.toggle("change");
+    this.menubgTarget.classList.toggle("change-bg");
+  }
+}
