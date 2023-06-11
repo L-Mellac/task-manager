9129723eb981d59b36422043821b9c55cf9fc296
Su Lynn Eu <eusulynn@gmail.com>
2023-06-11 12:36:39 +0100

    got my hamburger buttont to click without messing up my search bar

--- app/javascript/controllers/hotdog_navigation_progress_controller.js
+++ app/javascript/controllers/hotdog_navigation_progress_controller.js
@@ -2,16 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="hotdog-navigation"
 export default class extends Controller {
+  
   static targets = ["threeBars", "todayLink", "customizeLink", "circle1", "circle2", "circle3", "circle4"];
-
   toggleMenu() {
     this.threeBarsTarget.classList.toggle("hidden");
-    this.progressLinkTarget.classList.toggle("hidden");
+    this.todayLinkTarget.classList.toggle("hidden");
     this.customizeLinkTarget.classList.toggle("hidden");
-
-    this.circle1Target.classList.toggle("moved");
-    this.circle2Target.classList.toggle("moved");
-    this.circle3Target.classList.toggle("moved");
-    this.circle4Target.classList.toggle("moved");
+    this.circle1Target.classList.toggle("moved1");
+    this.circle2Target.classList.toggle("moved2");
+    this.circle3Target.classList.toggle("moved3");
+    this.circle4Target.classList.toggle("moved4");
   }
 }
