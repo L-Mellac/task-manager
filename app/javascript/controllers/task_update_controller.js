6b89824b88b367af492700e2bd26a32bab8c650d
L-Mellac <louismellac@gmail.com>
2023-06-12 21:26:56 +0100

    Added a button to change priority that shows up below the delete button when you hover. Linked to the task-update Stimulus controller but doesn't do anything yet

--- app/javascript/controllers/task_update_controller.js
+++ app/javascript/controllers/task_update_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="task-update"
 export default class extends Controller {
-  static targets = ["categoriesPopup", "deleteButton"]
+  static targets = ["categoriesPopup", "deleteButton", "priorityButton"]
   static values = { taskId: Number }
 
   connect() {
@@ -13,8 +13,9 @@ export default class extends Controller {
     this.categoriesPopupTarget.classList.toggle("hidden");
   }
 
-  toggleDeleteButton() {
+  togglePopups() {
     this.deleteButtonTarget.classList.toggle("hidden");
+    this.priorityButtonTarget.classList.toggle("hidden");
   }
 
   deleteTask(event) {
