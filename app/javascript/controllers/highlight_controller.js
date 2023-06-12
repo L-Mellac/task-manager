4b53836d30e757cddfa9694a625cc17e43ba9cef
L-Mellac <louismellac@gmail.com>
2023-06-12 20:18:22 +0100

    Now once you select a file, it gets set as the highlight

--- app/javascript/controllers/highlight_controller.js
+++ app/javascript/controllers/highlight_controller.js
@@ -2,11 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="highlight"
 export default class extends Controller {
-  connect() {
-    console.log("Hello, Stimulus!", this.element)
-  }
+  static targets = [ "highlightForm" ]
 
-  uploadHighlight(event) {
-    
+  uploadHighlight() {
+    this.highlightFormTarget.submit()
   }
 }
