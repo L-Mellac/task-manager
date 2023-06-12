7b2498d0f9efc068c0e3aaf41129ca134d9f1018
Louis Mellac <louismellac@gmail.com>
2023-06-12 12:15:13 +0100

    Delete button is now a nice icon and only shows up when you hover on a task

--- app/javascript/controllers/task_update_controller.js
+++ app/javascript/controllers/task_update_controller.js
@@ -2,17 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="task-update"
 export default class extends Controller {
-  static targets = ["categoriesPopup"]
+  static targets = ["categoriesPopup", "deleteBtn"]
 
   connect() {
     console.log("hello")
   }
 
   toggleCategoriesPopup() {
-    this.categoriesPopupTarget.classList.toggle("hidden")
+    this.categoriesPopupTarget.classList.toggle("hidden");
   }
 
-  updateCategory(event) {
+  showDeleteButton() {
+    this.deleteBtnTarget.classList.remove("hidden");
+  }
 
+  hideDeleteButton() {
+    console.log("hiding delete button");
+    this.deleteBtnTarget.classList.add("hidden");
   }
 }
