1526e36f27e14c02aac70ff7a7c9e43b60e878e2
Louis Mellac <louismellac@gmail.com>
2023-06-13 16:41:09 +0100

    The menu of the today page now only opens when you click on the hotdog and closes when you click anywhere else. Not yet on other pages though.

--- app/javascript/controllers/hotdog_navigation_controller.js
+++ app/javascript/controllers/hotdog_navigation_controller.js
@@ -4,7 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["threeBars", "progressLink", "customizeLink", "circle1", "circle2", "circle3", "circle4"];
 
-  toggleMenu() {
+  showMenu() {
     this.threeBarsTarget.classList.toggle("hidden");
     this.progressLinkTarget.classList.toggle("hidden");
     this.customizeLinkTarget.classList.toggle("hidden");
@@ -13,5 +13,19 @@ export default class extends Controller {
     this.circle2Target.classList.toggle("moved");
     this.circle3Target.classList.toggle("moved");
     this.circle4Target.classList.toggle("moved");
+  }
+
+  hideMenu(event) {
+    console.log(event.target)
+    if (event.target.id != "hotdog-menu" && event.target.parentElement.id != "hotdog-menu") {
+      this.threeBarsTarget.classList.remove("hidden");
+      this.progressLinkTarget.classList.add("hidden");
+      this.customizeLinkTarget.classList.add("hidden");
+
+      this.circle1Target.classList.remove("moved");
+      this.circle2Target.classList.remove("moved");
+      this.circle3Target.classList.remove("moved");
+      this.circle4Target.classList.remove("moved");
+    }
   }
 }
