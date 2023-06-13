fad6d18f4d852271e1cf4bbbc4416eacb439f1a1
Louis Mellac <louismellac@gmail.com>
2023-06-13 11:56:25 +0100

    Color picker now updates the color of the category once you choose one and press OK. Done badly with AJAX so you need to refresh to see the change.

---
+++ app/javascript/controllers/category_color_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="category"
+export default class extends Controller {
+  static targets = [ "colorSelectorPopup", "colorPickerInput" ]
+  static values = { categoryId: Number }
+
+  connect() {
+    this.csrfHeader = {"X-CSRF-Token": document.getElementsByName("csrf-token")[0].content}
+  }
+
+  toggleColorPopup() {
+    this.colorSelectorPopupTarget.classList.toggle("hidden");
+  }
+
+  submitColorChange() {
+    const color = this.colorPickerInputTarget.value;
+    fetch(`/categories/${this.categoryIdValue}`,
+    {
+      method: 'PATCH',
+      headers: {
+        ...this.csrfHeader,
+        'Content-Type': 'application/json'},
+      body: JSON.stringify({ category: {color: color} })
+    })
+  }
+}
