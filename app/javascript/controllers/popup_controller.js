c9c23923f25c802c3542ef873e7737428941be00
saroj pun <sarojpun@hotmail.com>
2023-06-13 09:13:04 +0100

    hamburger slanted straight, log in color, font size in todays page, today. to edit

--- app/javascript/controllers/popup_controller.js
+++ app/javascript/controllers/popup_controller.js
@@ -1,10 +1,12 @@
-import { Controller } from "@hotwired/stimulus"
+// import { Controller } from "@hotwired/stimulus"
 
-export default class extends Controller {
+// export default class extends Controller {
 
-  static targets = [ "popup" ]
+//   static targets = [ "popup" ]
 
-    togglePopup() {
-      this.element.remove();
-  }
-}
+//     togglePopup() {
+//       this.element.remove();
+//   }
+// }
+
+
