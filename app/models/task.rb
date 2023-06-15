843a1beb52bc9ae9186851eaeb447f0944aaba20
Su Lynn Eu <eusulynn@gmail.com>
2023-06-15 10:11:22 +0100

    checkin if the validations are causing the heroku error

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,5 @@ class Task < ApplicationRecord
   belongs_to :category, optional: true
 
   validates :description, presence: true
-  validates :priority, presence: true, numericality: { only_integer: true, in: (1..3) }
+  # validates :priority, presence: true, numericality: { only_integer: true, in: (1..3) }
 end
