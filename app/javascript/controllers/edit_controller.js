62c728e53f929c5ba8866825d7d149496a398452
saroj pun <sarojpun@hotmail.com>
2023-06-15 09:07:32 +0100

    edit changed and border bottom under task in today page

--- app/javascript/controllers/edit_controller.js
+++ app/javascript/controllers/edit_controller.js
@@ -2,10 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  static targets = [ "dayNameInput", "editIcon" ]
+  static targets = [ "dayNameInput" ]
 
   edit() {
     this.dayNameInputTarget.disabled = false;
-    this.editIconTarget.style.display ="none";
   }
 }
