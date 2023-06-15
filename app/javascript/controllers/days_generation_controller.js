04797fcc65eb7a1d1d7aed5fc035a9d82f9ce2bb
Louis Mellac <louismellac@gmail.com>
2023-06-15 11:22:09 +0100

    You can signup now, and it redirects you to a fresh new empty today page.

--- app/javascript/controllers/days_generation_controller.js
+++ app/javascript/controllers/days_generation_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="days-generation"
 export default class extends Controller {
-  static targets = ["username", "password"]
+  static targets = ["username", "password", "emailSignup", "passwordSignup"]
 
   connect() {
     console.log("Hello world")
@@ -10,6 +10,7 @@ export default class extends Controller {
   }
 
   loginAndGenerateMissingDays(event) {
+    console.log("loginAndGenerateMissingDays");
     event.preventDefault();
     const username = this.usernameTarget.value;
     const password = this.passwordTarget.value;
@@ -21,6 +22,47 @@ export default class extends Controller {
         'Content-Type': 'application/json'},
       body: JSON.stringify({ user: {email: username, password: password} })
     })
+    .then((response) => {
+      console.log(response);
+      fetch(`/days`,
+      {
+        method: 'POST',
+        headers: {
+          ...this.csrfHeader,
+          'Accept': 'text/plain'}
+      })
+      .then(response => response.text())
+      .then((last_day_id) => {
+        window.location = `/days/${last_day_id}`;
+      })})
+  }
+
+  signupAndGenerateMissingDays(event) {
+    console.log("signupAndGenerateMissingDays");
+    event.preventDefault();
+    const username = this.emailSignupTarget.value;
+    const password = this.passwordSignupTarget.value;
+    console.log(username);
+    console.log(password);
+    fetch(`/user`,
+    {
+      method: 'POST',
+      headers: {
+        ...this.csrfHeader,
+        'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        user: {email: username, password: password},
+        registration: {email: username, password: password}
+      })
+    })
+    .then((response) => {fetch(`/user/sign_in`,
+    {
+      method: 'POST',
+      headers: {
+        ...this.csrfHeader,
+        'Content-Type': 'application/json'},
+      body: JSON.stringify({ user: {email: username, password: password} })
+    })})
     .then((response) => {
       console.log(response);
       fetch(`/days`,
